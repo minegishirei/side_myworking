2b5f2739666fa67b52b43ae53fa9a54214d59738
みねぎしれい <68362169+minegishirei@users.noreply.github.com>
2023-07-04 09:37:27 +0900

    save

--- myworking/clone.sh
+++ myworking/clone.sh
@@ -3,5 +3,3 @@ git clone https://github.com/minegishirei/techblog.git
 git clone https://github.com/minegishirei/myworking.git
 git clone https://github.com/minegishirei/psy.git
 
-git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git
-./install.sh SourceCodePro
