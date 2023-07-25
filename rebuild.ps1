0330ea814dcac017adc2a83a35cc690b55bef0ce
minegishirei <minegishirei@gmail.com>
2023-07-25 00:31:52 +0000

    save

--- rebuild.ps1
+++ rebuild.ps1
@@ -1,5 +1,7 @@
+docker-comopse down -v
 cd ..
 Remove-Item -Recurse -Force ./myworking
 git clone https://github.com/minegishirei/myworking.git
 cd myworking
-docker-compose run myworking bash
+docker-compose build .
+docker-compose run myworking fish
