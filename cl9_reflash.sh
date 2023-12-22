31174f7888b5b9cd20701811cfafd33509381cca
みねぎしれい <68362169+minegishirei@users.noreply.github.com>
2023-12-22 11:18:43 +0900

    save

--- cl9_reflash.sh
+++ cl9_reflash.sh
@@ -2,5 +2,5 @@ cd ..
 sudo rm -rf ./*
 git clone https://github.com/minegishirei/myworking
 cd myworking
-docker-compose build -f cl9-docker-compose.yml
-docker-compose run myworking -f cl9-docker-compose.yml bash
+docker-compose -f cl9-docker-compose.yml build 
+docker-compose -f cl9-docker-compose.yml run myworking bash
