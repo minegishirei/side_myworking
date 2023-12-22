36999466a7c8e11e38fa78e8ec6409d8f6a421a4
みねぎしれい <68362169+minegishirei@users.noreply.github.com>
2023-12-22 11:15:26 +0900

    save

--- cl9_reflash.sh
+++ cl9_reflash.sh
@@ -3,4 +3,4 @@ sudo rm -rf ./*
 git clone https://github.com/minegishirei/myworking
 cd myworking
 docker-compose build
-docker-compose run myworking bash
+docker-compose run myworking --volume ~/.aws/:/root/.aws/ bash
