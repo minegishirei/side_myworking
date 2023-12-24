753945103f0c4ef72b6d20c3bc721cb354cc8858
みねぎしれい <68362169+minegishirei@users.noreply.github.com>
2023-12-24 19:34:29 +0900

    save

--- personal_reflash.sh
+++ personal_reflash.sh
@@ -1,6 +1,5 @@
 cd ..
 sudo rm -rf ./*
-git clone https://github.com/minegishirei/myworking
 cd myworking
 docker compose -f personal-docker-compose.yml build 
 docker compose -f personal-docker-compose.yml run myworking bash
