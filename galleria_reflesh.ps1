2dc11bc8a3726f6d11cd30e5e3a1ecba192017fa
みねぎしれい <68362169+minegishirei@users.noreply.github.com>
2024-01-15 08:54:57 +0900

    save

--- galleria_reflesh.ps1
+++ galleria_reflesh.ps1
@@ -1,1 +1,6 @@
+cd ..
 Remove-Item  ./*  -Recurse -Force
+git clone https://github.com/minegishirei/myworking
+cd myworking
+docker-compose -f business-docker-compose.yml build 
+docker-compose -f business-docker-compose.yml run myworking bash
