82356a4be5ca688bf60bf02c6a4f16c0c2cf6542
みねぎしれい <68362169+minegishirei@users.noreply.github.com>
2024-01-22 12:04:58 +0900

    save

--- root/.config/nvim/init.lua
+++ root/.config/nvim/init.lua
@@ -1,26 +1,3 @@
-if vim.loader and vim.fn.has "nvim-0.9.1" == 1 then vim.loader.enable() end
-
-for _, source in ipairs {
-  "astronvim.bootstrap",
-  "astronvim.options",
-  "astronvim.lazy",
-  "astronvim.autocmds",
-  "astronvim.mappings",
-} do
-  local status_ok, fault = pcall(require, source)
-  if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
-end
-
-if astronvim.default_colorscheme then
-  if not pcall(vim.cmd.colorscheme, astronvim.default_colorscheme) then
-    require("astronvim.utils").notify(
-      ("Error setting up colorscheme: `%s`"):format(astronvim.default_colorscheme),
-      vim.log.levels.ERROR
-    )
-  end
-end
-
-require("astronvim.utils").conditional_func(astronvim.user_opts("polish", nil, false), true)
 
 
 vim.keymap.set('n', 'h', 'hzz')
@@ -46,3 +23,29 @@ vim.keymap.set('n', 'sb', ':<C-u>bd!<CR>')
 
 -- set ESC+ESC Clear Search hilight
 vim.keymap.set('n', '<ESC><ESC>', ':<C-u>set nohlsearch!<CR>')
+
+
+
+if vim.loader and vim.fn.has "nvim-0.9.1" == 1 then vim.loader.enable() end
+
+for _, source in ipairs {
+  "astronvim.bootstrap",
+  "astronvim.options",
+  "astronvim.lazy",
+  "astronvim.autocmds",
+  "astronvim.mappings",
+} do
+  local status_ok, fault = pcall(require, source)
+  if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
+end
+
+if astronvim.default_colorscheme then
+  if not pcall(vim.cmd.colorscheme, astronvim.default_colorscheme) then
+    require("astronvim.utils").notify(
+      ("Error setting up colorscheme: `%s`"):format(astronvim.default_colorscheme),
+      vim.log.levels.ERROR
+    )
+  end
+end
+
+require("astronvim.utils").conditional_func(astronvim.user_opts("polish", nil, false), true)
