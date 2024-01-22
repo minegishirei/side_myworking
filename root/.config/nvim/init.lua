df8d7492e3cc07183117c8e46082c319f5389b92
みねぎしれい <68362169+minegishirei@users.noreply.github.com>
2024-01-22 11:37:49 +0900

    save

--- root/.config/nvim/init.lua
+++ root/.config/nvim/init.lua
@@ -21,3 +21,38 @@ if astronvim.default_colorscheme then
 end
 
 require("astronvim.utils").conditional_func(astronvim.user_opts("polish", nil, false), true)
+
+
+vim.keymap.set('n', 'h', 'hzz')
+vim.keymap.set('n', 'j', 'jzz')
+
+
+map <S-h> 10hzz
+map <S-j> 5jzz
+map <S-k> 10kzz
+map <S-l> 5lzz
+map h hzz
+map j jzz
+map k kzz
+map l lzz
+
+-- set s key to Window moving.
+vim.keymap.set('n', 's', '<NOP>')
+-- set s+hjkl move to other window
+vim.keymap.set('n', 'sj', '<C-w>j')
+vim.keymap.set('n', 'sk', '<C-w>k')
+vim.keymap.set('n', 'sl', '<C-w>l')
+vim.keymap.set('n', 'sh', '<C-w>h')
+-- set s+sv separate window
+vim.keymap.set('n', 'ss', '<C-w>s')
+vim.keymap.set('n', 'sv', '<C-w>v')
+-- set s+nptT move tab
+vim.keymap.set('n', 'sn', 'gt')
+vim.keymap.set('n', 'sp', 'gT')
+vim.keymap.set('n', 'st', ':<C-u>tabnew<CR>')
+-- set s+qQ kill buffer or close window
+vim.keymap.set('n', 'sq', ':<C-u>q<CR>')
+vim.keymap.set('n', 'sb', ':<C-u>bd!<CR>')
+
+-- set ESC+ESC Clear Search hilight
+vim.keymap.set('n', '<ESC><ESC>', ':<C-u>set nohlsearch!<CR>')
