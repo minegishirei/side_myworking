eb410e8f8c5e4a19ca5f5c88e3892daa6930f8cb
みねぎしれい <68362169+minegishirei@users.noreply.github.com>
2024-08-17 19:39:20 +0900

    save

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -2,3 +2,6 @@
 # security tools
 brew install sqlmap
 
+# vim plugin manager
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
