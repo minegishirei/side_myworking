70bed76b4e63b04f5a9861aa5be4348e739c9b7a
峰岸零 <minegishirei@minegishireinoMacBook-Air.local>
2025-02-08 20:13:59 +0900

    edit file

--- brew.sh
+++ brew.sh
@@ -37,7 +37,7 @@ install_if_missing "gh"
 install_if_missing "eza"
 
 install_if_missing "tmux"
-install_if_missing "eza"
+install_if_missing ""
 
 ## for editor
 install_if_missing visual-studio-code --cask
