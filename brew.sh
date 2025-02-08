9ad31a4b948b26a5882d2efff3abf31886c6cc17
峰岸零 <minegishirei@minegishireinoMacBook-Air.local>
2025-02-08 09:45:56 +0900

    save

--- brew.sh
+++ brew.sh
@@ -1,40 +1,42 @@
 #!/bin/bash
 
-# install brew
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 
 # 関数：コマンドがインストールされているかチェック
 install_if_missing() {
-  if ! command -v "$1" &> /dev/null; then
+  if ! brew list "$1" &> /dev/null; then
     echo "$1 が見つかりません。インストールを開始します..."
-    brew install "$1"
+    brew install "$1" "${@:2:3}"
   else
     echo "$1 はすでにインストールされています。スキップします。"
   fi
 }
 
-## for editor
-brew install --cask visual-studio-code
+# install brew
+if ! command -v "brew" &> /dev/null; then
+    echo "brew が見つかりません。インストールを開始します..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "brew はすでにインストールされています。スキップします。"
+fi
 
+# awscli のインストール
+install_if_missing "awscli"
+# sqlmap のインストール
+install_if_missing "sqlmap"
+# gobuster のインストール
+install_if_missing "gobuster"
+# git のインストール
+install_if_missing "git"
+# gh (GitHub CLI) のインストール
+install_if_missing "gh"
+# eza のインストール
+install_if_missing "eza"
+## for editor
+install_if_missing visual-studio-code --cask
 ### Install Extensions
 code --install-extension vscodevim.vim
 code --install-extension hediet.vscode-drawio
 
+install_if_missing "ccat"
 
-# awscli のインストール
-install_if_missing "awscli"
-
-# sqlmap のインストール
-install_if_missing "sqlmap"
-
-# gobuster のインストール
-install_if_missing "gobuster"
-
-# git のインストール
-install_if_missing "git"
-
-# gh (GitHub CLI) のインストール
-install_if_missing "gh"
-
-# exa のインストール
-install_if_missing "exa"
--- dotfiles/.bashrc
+++ dotfiles/.bashrc
@@ -1,4 +1,4 @@
 set -o vi
 alias ggit="git add . && git commit -m 'save' && git push"
-alias ls="exa" #need comment
-
+alias ls="eza" #need comment
+alias cat="ccat"
