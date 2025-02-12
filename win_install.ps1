d4d129a39837d4d08b1403ba19ab6595bed5ca37
峰岸零 <minegishirei@minegishireinoMacBook-Air.local>
2025-02-12 13:17:09 +0900

    test

--- win_install.ps1
+++ win_install.ps1
@@ -1,4 +1,2 @@
-cp ${HOME}\side_myworking\dotfiles\settings.json %APPDATA%\Code\User\settings.json
-cp ${HOME}\side_myworking\dotfiles\keybindings.json %APPDATA%\Code\User\keybindings.json
-code --install-extension vscodevim.vim
-code --install-extension hediet.vscode-drawio
+cp ${HOME}\side_myworking\dotfiles\settings.json $env:APPDATA\Code\User\settings.json
+cp ${HOME}\side_myworking\dotfiles\keybindings.json $env:APPDATA\Code\User\keybindings.json
