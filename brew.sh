26ffee4b60fc2ca0cf4688f73f302d41d5d290dc
峰岸零 <minegishirei@minegishireinoMacBook-Air.local>
2025-03-05 19:25:34 +0900

    test

--- brew.sh
+++ brew.sh
@@ -34,6 +34,9 @@ install_if_missing "ccat"
 install_if_missing "fswatch"
 install_if_missing "starship"
 install_if_missing "driftctl"
+install_if_missing "btop"
+install_if_missing "ctop"
+install_if_missing "yazi"
 
 ## for editor
 install_if_missing visual-studio-code --cask
