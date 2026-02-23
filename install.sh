05b47bb77034946c31af98f04ca002d422ef91df
みねぎしれい <68362169+minegishirei@users.noreply.github.com>
2026-02-23 20:07:01 +0900

    Initialize Homebrew in .zprofile and clean directory
    Add Homebrew initialization to .zprofile and clean up.

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+echo >> $HOME/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv zsh)"' >> $HOME/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv zsh)"
+
 # もし `side_myworking` が存在すれば削除
 rm -rf side_myworking
 rmdir side_myworking
